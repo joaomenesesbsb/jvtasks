fecfdb50702fba01c8ebfbde6a8671813641526b
Joao <joaovic2012@hotmail.com>
2023-04-04 14:47:21 -0300

    first commit

---
+++ backend-jvtasks/src/main/java/com/jvmeneses/jvtasks/JvtasksApplication.java
@@ -0,0 +1,13 @@
+package com.jvmeneses.jvtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvtasksApplication.class, args);
+	}
+
+}
---
+++ backend-jvtasks/src/test/java/com/jvmeneses/jvtasks/JvtasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmeneses.jvtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvtasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
